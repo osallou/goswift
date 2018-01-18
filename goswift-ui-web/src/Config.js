b418a4bcc635726fe87cea5669fa24c6175f9792
Olivier Sallou <olivier.sallou@gmail.com>
2018-01-18 14:47:14 +0100

    use REACT_APP_GOSWIFT_BACKEND_URL env variable for backend

--- goswift-ui-web/src/Config.js
+++ goswift-ui-web/src/Config.js
@@ -3,7 +3,7 @@ import { Component } from 'react';
 class Config extends Component {
   static getConfig(){
       return {
-          'url': 'http://localhost:6543'
+          'url': process.env.REACT_APP_GOSWIFT_BACKEND_URL || ''
       }
   }
   render() {
